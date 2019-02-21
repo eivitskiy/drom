df5c491585a4f53278766f86cf21384eca151e83
eivitskiy <eivitskiy@gmail.com>
2019-02-21 11:28:47 +0700

    CHG: - reconfigured docker - added composer container - mod_rewrite enabled

--- www/public/index.php
+++ www/public/index.php
@@ -1,3 +1,12 @@
 <?php
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+define("ROOT_PATH", $_SERVER['DOCUMENT_ROOT'] . '/..');
+define("APP_PATH", ROOT_PATH . '/application');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
 phpinfo();
